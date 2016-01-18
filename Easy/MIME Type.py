c8812a587eaa23c1d21907a038f293c2d61c0225
Brian Thys <brianthys@hotmail.com>
2016-01-18 22:38:56 +0100

    Update MIME Type.py

--- Easy/MIME Type.py
+++ Easy/MIME Type.py
@@ -5,28 +5,29 @@ import math
 # the standard input according to the problem statement.
 
 n = int(raw_input())  # Number of elements which make up the association table.
-q = int(raw_input())  # Number Q of file names to be analyzed
-ext_list = []
-file_name_list = []
-fname_list = []
-
+q = int(raw_input())  # Number Q of file names to be analyzed.
+ext = []
+mt = []
 for i in xrange(n):
-    ext, mt = raw_input().split()
+    # ext: file extension
+    # mt: MIME type.
+    regel = raw_input().split()
+    ext.append(regel[0].lower())
+    mt.append(regel[1])
 
-    file_name_list.append(mt)
-    ext_list.append("." + ext.lower())
-
-#print >> sys.stderr, ext_list
-#print >> sys.stderr, file_name_list
-
+fname = []
 for i in xrange(q):
-    fname = raw_input().lower()  # One file name per line.
-    fname = fname[fname.find("."):].replace("..", ".")
-    
-    #print fname
-    #print ext_list.count(fname)
+    fname.append(raw_input().lower())  # One file name per line.
+# Write an action using print
+# To debug: print >> sys.stderr, "Debug messages..."
+teller = 0
+# For each of the Q filenames, display on a line the corresponding MIME type. If there is no corresponding type, then display UNKNOWN.
+for fn in fname:
 
-    if ext_list.count(fname) == 0:
-        print "UNKNOWN"
+    k = fn.rfind(".")
+    extens = fn[k + 1:]
+    if k >= 0 and extens in ext:
+        i = ext.index(extens)
+        print mt[i]
     else:
-        print file_name_list[ext_list.index(fname)]
+        print "UNKNOWN"
